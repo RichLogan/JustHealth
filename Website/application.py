22544ee4f218c3b57cba2cd33d4aa87caad5828d
charlottehutchinson <ch446@kent.ac.uk>
2014-10-09 17:44:41 +0100

    Template Design

--- Website/application.py
+++ Website/application.py
@@ -20,6 +20,10 @@ def registration():
     else:
       return render_template('register.html')
 
+@app.route('/login')
+def login():
+  return render_template('login.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
