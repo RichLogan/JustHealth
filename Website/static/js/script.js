ac38aed72186aa40b9fd98723928347c2c6cd28e
charlottehutchinson <ch446@kent.ac.uk>
2014-10-11 11:16:32 +0100

    javascript

---
+++ Website/static/js/script.js
@@ -0,0 +1,32 @@
+$(window).resize (
+    function () {
+        layout();
+    }
+);
+
+
+function validateForm() {
+    var x = document.forms["register"]["username","firstName","surname","dob", "email", "confirmPassword", "password"].value;
+    if (x==null || x=="") {
+        alert("All fields be filled out");
+    }
+}
+
+
+function validateEmail() {
+    var x = document.forms["register"]["email"].value;
+    var atpos = x.indexOf("@");
+    var dotpos = x.lastIndexOf(".");
+    if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length) {
+        alert("Not a valid e-mail address");
+        return false;
+    }
+}
+
+function validateDob() {
+  var pattern =/^([0-9]{2})-([0-9]{2})-([0-9]{4})$/;
+  if(pattern.test(dob) ==false {
+     alert("Invalid date");
+     return false;
+   }
+}
