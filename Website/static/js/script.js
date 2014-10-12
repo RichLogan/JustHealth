451eaf67ffc8d81730daf6281c390cfaa45df3e6
Stephen Tate <steve.tate4@gmail.com>
2014-10-12 14:47:17 +0100

    changes to js, not working innerHTML

--- Website/static/js/script.js
+++ Website/static/js/script.js
@@ -18,7 +18,8 @@ function validateEmail() {
     var atpos = x.indexOf("@");
     var dotpos = x.lastIndexOf(".");
     if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length) {
-        alert("Not a valid e-mail address");
+        document.getElementById('errorEmail').innerHTML = "Not a valid e-mail address";
+		alert("Not a valid e-mail address");
         return false;
     }
 }
