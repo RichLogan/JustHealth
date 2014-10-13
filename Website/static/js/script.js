e02acff0d857bcfd0d5350e8cebee1a701ad1e40
benmcgregor <ben.mcgregor1@gmail.com>
2014-10-13 15:14:28 +0100

    Password Strength Check

--- Website/static/js/script.js
+++ Website/static/js/script.js
@@ -1,8 +1,8 @@
-$(window).resize (
-    function () {
-        layout();
-    }
-);
+//$(window).resize (
+//    function () {
+//        layout();
+//    }
+//);
 
 //check form fields are not null
 function validateForm() {
@@ -31,4 +31,62 @@ function validateDob() {
      alert("Invalid date");
      return false;
    }
+}
+
+/*
+check password strength 
+(http://runnable.com/UfJrnXtk2tZXAAA1/how-to-check-password-strength-using-jquery)
+*/
+//password checking function
+	
+function checkStrength(password)
+{
+	//default strength
+	var strength = 0
+	
+	//if the password length is less than 6, return message
+	if (password.length < 6) { 
+		$('#result').removeClass()
+		$('#result').addClass('short')
+		return 'Too short' 
+	}
+	
+	//length is okay, continue.
+	
+	//if length is 8 characters or more, increase strength value
+	if (password.length > 7) strength += 1
+	
+	//if password contains both lower and uppercase characters, increase strength value
+	if (password.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/))  strength += 1
+	
+	//if it has numbers and characters, increase strength value
+	if (password.match(/([a-zA-Z])/) && password.match(/([0-9])/))  strength += 1 
+	
+	//if it has one special character, increase strength value
+	if (password.match(/([!,%,&,@,#,$,^,*,?,_,~])/))  strength += 1
+	
+	//if it has two special characters, increase strength value
+	if (password.match(/(.*[!,%,&,@,#,$,^,*,?,_,~].*[!,%,&,@,#,$,^,*,?,_,~])/)) strength += 1
+	
+	//return messages on strength value
+	
+	//if value is less than 2
+	if (strength < 2 )
+	{
+		$('#result').removeClass();
+		$('#result').addClass('weak');
+		return 'Weak'			
+	}
+	else if (strength == 2 )
+	{
+		$('#result').removeClass();
+		$('#result').addClass('good');
+		return 'Good'		
+	}
+	else
+	{
+		$('#result').removeClass();
+		$('#result').addClass('strong');
+		return 'Strong'
+	}
 }
