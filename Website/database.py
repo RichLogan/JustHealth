bbc2fe6d40660cd1c9c20b5a61ed5e0ee60d16cf
Rich Logan <accounts@richlogan.co.uk>
2014-10-13 09:31:47 +0100

    Generated from pwiz

--- Website/database.py
+++ Website/database.py
@@ -1,27 +1,34 @@
 from peewee import *
 
-db = PostgresqlDatabase("justhealth", host="penguin.kent.ac.uk",port=5432,user="justhealth",password="dsomoid")
+database = PostgresqlDatabase('justhealth', **{'host': 'penguin.kent.ac.uk', 'password': 'dsomoid', 'port': 5432, 'user': 'justhealth'})
 
-class Client(Model):
-    username = CharField(primary_key=True)
-    firstname = CharField()
-    surname = CharField()
-    dob = DateField()
-    ismale = BooleanField()
-    iscarer = BooleanField()
-    email = CharField()
-    verified = BooleanField()
+class UnknownField(object):
+    pass
+
+class BaseModel(Model):
+    class Meta:
+        database = database
+
+class Client(BaseModel):
     accountlocked = BooleanField()
+    dob = DateField()
+    email = CharField(max_length=100)
+    firstname = CharField(max_length=100)
+    iscarer = BooleanField()
+    ismale = BooleanField()
+    surname = CharField(max_length=100)
+    username = CharField(max_length=25, primary_key=True)
+    verified = BooleanField()
 
     class Meta:
-      database = db
+        db_table = 'client'
 
-class uq8LnAWi7D(Model):
+class Uq8Lnawi7D(BaseModel):
+    expirydate = DateField(null=True)
+    iscurrent = BooleanField(null=True)
+    password = CharField(max_length=255)
     recordid = PrimaryKeyField()
-    username = CharField()
-    password = CharField()
-    iscurrent = BooleanField()
-    expirydate = DateField()
+    username = ForeignKeyField(db_column='username', rel_model=Client, to_field='username')
 
     class Meta:
-        database = db
+        db_table = 'uq8lnawi7d'
