ae5596d02b179f0154d570b20ec3b65db10f5ed6
charlottehutchinson <ch446@kent.ac.uk>
2014-10-13 10:30:14 +0100

    test

--- Website/application.py
+++ Website/application.py
@@ -42,8 +42,8 @@ def registration():
       for key in profile:
         profile[key] = profile[key].strip()
       # Validate fields not null
-    #  if (profile['username'] or profile['firstname'] or profile['surname'] or profile['dob'] or profile['email'] or profile['password'] or profile['confirm password'] == None):
-    #    return 'All fields must be filled in'
+      if (profile['username'] or profile['firstname'] or profile['surname'] or profile['dob'] or profile['email'] or profile['password'] or profile['confirm password'] == None):
+        return 'All fields must be filled in'
 
       # Validate username >25
       if (len(profile['username']) >25):
