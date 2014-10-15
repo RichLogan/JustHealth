9efcd6b4f2404cd7774ff4f729a798adb5026b4e
Rich Logan <accounts@richlogan.co.uk>
2014-10-15 17:07:12 +0100

    Adding missing transaction support

--- Iteration 1/Test Cases/Testing Scripts/testCase_1_22.py
+++ Iteration 1/Test Cases/Testing Scripts/testCase_1_22.py
@@ -16,29 +16,29 @@ class testCase_1_22(unittest.TestCase):
     deletePasswords.execute()
 
   def test_1_22_1(self):
-    newUser = testDatabase.Client.insert(
-      username= 'test',
-      firstname='test',
-      surname='test',
-      dob='01/01/2001',
-      ismale='TRUE',
-      iscarer='TRUE',
-      email='test@test.com')
-    newUser.execute()
-    newPassword = testDatabase.uq8LnAWi7D.insert(
-      username ='test',
-      password ='password',
-      iscurrent = 'TRUE',
-      expirydate = '10/10/2014')
-    newPassword.execute()
-    #We need to check that the records are inserted correctly
-    self.assertEqual(testDatabase.Client.select().count(),1)
-    self.assertEqual(testDatabase.uq8LnAWi7D.select().count(),1)
-    clientDelete = testDatabase.Client.delete().where(testDatabase.Client.username =='test')
-    clientDelete.execute()
-    self.assertEqual(testDatabase.Client.select().count(),0)
-    self.assertEqual(testDatabase.uq8LnAWi7D.select().count(),0)
-
+    with testDatabase.database.transaction():
+      newUser = testDatabase.Client.insert(
+        username= 'test',
+        firstname='test',
+        surname='test',
+        dob='01/01/2001',
+        ismale='TRUE',
+        iscarer='TRUE',
+        email='test@test.com')
+      newUser.execute()
+      newPassword = testDatabase.uq8LnAWi7D.insert(
+        username ='test',
+        password ='password',
+        iscurrent = 'TRUE',
+        expirydate = '10/10/2014')
+      newPassword.execute()
+      #We need to check that the records are inserted correctly
+      self.assertEqual(testDatabase.Client.select().count(),1)
+      self.assertEqual(testDatabase.uq8LnAWi7D.select().count(),1)
+      clientDelete = testDatabase.Client.delete().where(testDatabase.Client.username =='test')
+      clientDelete.execute()
+      self.assertEqual(testDatabase.Client.select().count(),0)
+      self.assertEqual(testDatabase.uq8LnAWi7D.select().count(),0)
 
   def tearDown(self):
     deleteUsers = testDatabase.Client.delete()
