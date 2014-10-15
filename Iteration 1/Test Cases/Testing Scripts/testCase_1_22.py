d8ca31ffeb8ea2f83d446159fe26a702ed363456
Rich Logan <accounts@richlogan.co.uk>
2014-10-15 14:06:44 +0100

    Renaming

---
+++ Iteration 1/Test Cases/Testing Scripts/testCase_1_22.py
@@ -0,0 +1,50 @@
+# ORM Testing Script
+# Test Case 1_22
+
+from peewee import *
+import unittest
+import imp
+
+testDatabase = imp.load_source('testDatabase', '../../../Website/testDatabase.py')
+
+class testCase_1_22(unittest.TestCase):
+
+  def setUp(self):
+    deleteUsers = testDatabase.Client.delete()
+    deletePasswords = testDatabase.uq8LnAWi7D.delete()
+    deleteUsers.execute()
+    deletePasswords.execute()
+
+  def test_1_22_1(self):
+    newUser = testDatabase.Client.insert(
+      username= 'test',
+      firstname='test',
+      surname='test',
+      dob='01/01/2001',
+      ismale='TRUE',
+      iscarer='TRUE',
+      email='test@test.com')
+    newUser.execute()
+    newPassword = testDatabase.uq8LnAWi7D.insert(
+      username ='test',
+      password ='password',
+      iscurrent = 'TRUE',
+      expirydate = '10/10/2014')
+    newPassword.execute()
+    #We need to check that the records are inserted correctly
+    self.assertEqual(testDatabase.Client.select().count(),1)
+    self.assertEqual(testDatabase.uq8LnAWi7D.select().count(),1)
+    clientDelete = testDatabase.Client.delete().where(testDatabase.Client.username =='test')
+    #We need to check that the records are deleted from password and client table
+    self.assertEqual(testDatabase.Client.select().count(),0)
+    self.assertEqual(testDatabase.uq8LnAWi7D.select().count(),0)
+
+
+  def tearDown(self):
+    deleteUsers = testDatabase.Client.delete()
+    deletePasswords = testDatabase.uq8LnAWi7D.delete()
+    deleteUsers.execute()
+    deletePasswords.execute()
+
+if __name__ == '__main__':
+    unittest.main()
