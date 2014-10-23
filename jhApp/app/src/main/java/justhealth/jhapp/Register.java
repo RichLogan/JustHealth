53a78b5ae6b0b297d09e5afee6d44ee6567723c1
Rich Logan <accounts@richlogan.co.uk>
2014-10-23 18:33:22 +0100

    Working registration

--- jhApp/app/src/main/java/justhealth/jhapp/Register.java
+++ jhApp/app/src/main/java/justhealth/jhapp/Register.java
@@ -2,24 +2,137 @@ package justhealth.jhapp;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Spinner;
-import android.widget.ArrayAdapter;
-import android.R.*;
+import android.view.View;
+import android.widget.*;
 
+import java.util.Set;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+
+
+// Post Requests
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.HttpResponse;
+import java.io.IOException;
+import java.util.Map;
+
+import android.os.StrictMode;
+
+import org.apache.http.HttpEntity;
+import java.io.InputStream;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.util.EntityUtils;
 
 public class Register extends ActionBarActivity {
 
+    Button registerButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Spinner spinner = (Spinner) findViewById(R.id.accountType);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.accountType, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        registerButton = (Button)findViewById(R.id.register);
+
+        registerButton.setOnClickListener(
+                new View.OnClickListener() {
+                    public void onClick(View view) {
+
+                        sendRegister();
+                    }
+                }
+        );
+    }
+
+    private void sendRegister() {
+
+        HashMap<String, String> details = new HashMap<String, String>();
+
+        //Text Boxes
+        details.put("username", ((EditText) findViewById(R.id.username)).getText().toString());
+        details.put("firstname", ((EditText)findViewById(R.id.firstName)).getText().toString());
+        details.put("surname", ((EditText)findViewById(R.id.surname)).getText().toString());
+        details.put("dob", ((EditText) findViewById(R.id.dob)).getText().toString());
+        details.put("email", ((EditText)findViewById(R.id.email)).getText().toString());
+
+        //Gender
+        Boolean ismale = null;
+        int id = ((RadioGroup)findViewById(R.id.sex)).getCheckedRadioButtonId();
+        if (id == R.id.male) {
+            ismale = true;
+        }
+        else if (id == R.id.female) {
+            ismale = false;
+        }
+        details.put("ismale", ismale.toString());
+
+        //Account Type
+        //Needs Spinner working
+        Boolean accountType = false;
+        details.put("iscarer", accountType.toString());
+
+        String password = ((EditText)findViewById(R.id.password)).getText().toString();
+        String confirmPassword = ((EditText)findViewById(R.id.confirmPassword)).getText().toString();
+
+
+        if (((CheckBox)findViewById(R.id.tsandcs)).isChecked()) {
+                System.out.println("come on");
+                details.put("password", password);
+                details.put("confirmpassword", confirmPassword);
+
+            //TODO: set the terms and conditions properly
+            details.put("terms","on");
+
+                post(details);
+        }
+        else {
+                //Ts and cs not accepted
+        }
+    }
+
+    public void post(HashMap<String, String> details) {
+        // Create a new HttpClient and Post Header
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpPost httppost = new HttpPost("http://raptor.kent.ac.uk:5000/register");
+
+        try {
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+
+            Set<Map.Entry<String, String>> detailsSet = details.entrySet();
+            for (Map.Entry<String, String> s : detailsSet) {
+                System.out.println(s);
+                nameValuePairs.add(new BasicNameValuePair(s.getKey(), s.getValue()));
+            }
+
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+            HttpResponse response = httpclient.execute(httppost);
+            System.out.println(response.getStatusLine());
+
+            //HttpEntity responseEntity = response.getEntity();
+            //InputStream is = responseEntity.getContent();
+            //System.out.println(is.toString());
+            String responseStr = EntityUtils.toString(response.getEntity());
+            System.out.println(responseStr);
+
+
+        } catch (ClientProtocolException e) {
+            // TODO Auto-generated catch block
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+        }
     }
 
     @Override
