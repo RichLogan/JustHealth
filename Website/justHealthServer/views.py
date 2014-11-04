fa50470c22d73c66e36d08a5d2ad3a3bb6b0859c
benmcgregor <ben.mcgregor1@gmail.com>
2014-11-04 11:57:03 +0000

    update

--- Website/justHealthServer/views.py
+++ Website/justHealthServer/views.py
@@ -24,6 +24,9 @@ def index():
 def terms():
   return render_template('termsandconditions.html')
 
+@app.route('/deactivate')
+def deactivate():
+  return render_template('deactivate.html')
 
 # Account Pages
 @app.route('/register', methods=['POST', 'GET'])
