8be1e9d98a9f3188c25eb6091c362290931c4b44
benmcgregor <ben.mcgregor1@gmail.com>
2014-11-06 20:56:19 +0000

    deactivateConfirm function

--- Website/justHealthServer/static/js/script.js
+++ Website/justHealthServer/static/js/script.js
@@ -117,3 +117,12 @@ function terms()
         message.innerHTML = "<i class='fa fa-times'></i>"
     }
 }
+
+function deactivateConfirm(event) {
+
+	if (confirm('Are you sure you want to deactivate this account?')) {
+	
+	} else {
+		event.preventDefault();
+	}
+}
