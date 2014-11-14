0afec043e92aaed5c38734fb0c4dacebd4bcc72f
Stephen Tate <steve.tate4@gmail.com>
2014-11-14 01:08:09 +0000

    search GUI implemented

---
+++ jhApp/app/src/main/java/justhealth/jhapp/Search.java
@@ -0,0 +1,15 @@
+package justhealth.jhapp;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by Stephen Tate on 14/11/2014.
+ */
+public class Search extends ActionBarActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings);
+    }
+}
