a024757484965afc554ecf13fc40cbf5c5613a00
Stephen Tate <steve.tate4@gmail.com>
2014-11-26 11:41:18 +0000

    updated connections

--- jhApp/app/src/main/java/justhealth/jhapp/Connections.java
+++ jhApp/app/src/main/java/justhealth/jhapp/Connections.java
@@ -1,15 +1,46 @@
 package justhealth.jhapp;
 
+import android.annotation.TargetApi;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by Stephen on 25/11/2014.
  */
 public class Connections extends ActionBarActivity {
+
+    private int rowOfTable = 0;
 
     protected void onCreate(Bundle savedInstanceState) {
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
@@ -17,5 +48,226 @@ public class Connections extends ActionBarActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.connections);
+        getConnections();
+    }
+
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    private void getConnections() {
+        HashMap<String, String> getConnectionsInfo = new HashMap<String, String>();
+
+        //this is adding the username as null - INCORRECT
+        SharedPreferences account = getSharedPreferences("account", 0);
+        String username = account.getString("username", null);
+        String password = account.getString("password", null);
+
+        //add search to HashMap
+        getConnectionsInfo.put("username", username);
+
+        //Create new HttpClient and Post Header
+        HttpClient httpclient = new DefaultHttpClient();
+        String authentication = username + ":" + password;
+        String encodedAuthentication = Base64.encodeToString(authentication.getBytes(), Base64.NO_WRAP);
+
+        HttpPost httppost = new HttpPost("http://raptor.kent.ac.uk:5000/api/getConnections");
+        httppost.setHeader("Authorization", "Basic " + encodedAuthentication);
+        System.out.println(encodedAuthentication);
+        //assigns the HashMap to list, for post request encoding
+        try {
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+
+            Set<Map.Entry<String, String>> detailsSet = getConnectionsInfo.entrySet();
+            for (Map.Entry<String, String> string : detailsSet) {
+                nameValuePairs.add(new BasicNameValuePair(string.getKey(), string.getValue()));
+            }
+
+            //pass the list to the post request
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+            HttpResponse response = httpclient.execute(httppost);
+
+            String responseString = EntityUtils.toString(response.getEntity());
+            System.out.print(responseString);
+            JSONObject queryReturn = null;
+            JSONArray outgoing = null;
+            JSONArray incoming = null;
+            JSONArray completed = null;
+            try {
+                queryReturn = new JSONObject(responseString);
+                System.out.println(queryReturn);
+                String outgoingString = queryReturn.getString("outgoing");
+                outgoing = new JSONArray(outgoingString);
+                String incomingString = queryReturn.getString("incoming");
+                incoming = new JSONArray(incomingString);
+                String completedString = queryReturn.getString("completed");
+                completed = new JSONArray(completedString);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            System.out.print(queryReturn);
+            printTableHeader();
+            printOutgoingConnections(outgoing);
+            printIncomingConnections(incoming);
+
+        } catch (ClientProtocolException e) {
+            //TODO Auto-generated catch block
+        } catch (IOException e) {
+            //TODO Auto-generated catch block
+        } catch (NullPointerException e) {
+            //TODO Auto-generated catch block
+        }
+    }
+
+    private void printTableHeader() {
+        TableLayout searchTable = (TableLayout) findViewById(R.id.connectionsTable);
+
+        searchTable.removeAllViews();
+        //create heading row
+        TableRow head = new TableRow(this);
+        //add properties to the header row
+        head.setBackgroundColor(getResources().getColor(R.color.header));
+
+        //create the headings of the table
+        TextView headingUsername = new TextView(this);
+        headingUsername.setTextColor(Color.WHITE);
+        headingUsername.setPadding(5, 5, 5, 5);
+
+        TextView headingFirstName = new TextView(this);
+        headingFirstName.setTextColor(Color.WHITE);
+        headingFirstName.setPadding(5, 5, 5, 5);
+
+        TextView headingSurname = new TextView(this);
+        headingSurname.setTextColor(Color.WHITE);
+        headingSurname.setPadding(5, 5, 5, 5);
+
+        TextView headingCode = new TextView(this);
+        headingSurname.setTextColor(Color.WHITE);
+        headingSurname.setPadding(5, 5, 5, 5);
+
+        TextView headingAction = new TextView(this);
+        headingAction.setTextColor(Color.WHITE);
+        headingAction.setPadding(5, 5, 5, 5);
+
+        headingUsername.setText("Username");
+        headingFirstName.setText("First Name");
+        headingSurname.setText("Surname");
+        headingCode.setText("Code");
+        headingAction.setText("Action");
+
+        //add the headings to the row
+        head.addView(headingUsername);
+        head.addView(headingFirstName);
+        head.addView(headingSurname);
+        head.addView(headingCode);
+        head.addView(headingAction);
+        searchTable.addView(head, 0);
+    }
+
+    private void printOutgoingConnections(JSONArray outgoing){
+        TableLayout searchTable = (TableLayout) findViewById(R.id.connectionsTable);
+        for (int i = 0; i < outgoing.length(); i++) {
+            try {
+                JSONObject obj = outgoing.getJSONObject(i);
+                System.out.println(obj);
+                String outgoingUsername = obj.getString("username");
+                String outgoingFirstName = obj.getString("firstname");
+                String outgoingSurname = obj.getString("surname");
+                String outgoingCode = obj.getString("code");
+
+
+                TableRow row = new TableRow(this);
+                //add username to TextView
+                TextView forUsername = new TextView(this);
+                forUsername.setText(outgoingUsername);
+                //add first name to TextView
+                TextView forFirstName = new TextView(this);
+                forFirstName.setText(outgoingFirstName);
+                //add surname to TextView
+                TextView forSurname = new TextView(this);
+                forSurname.setText(outgoingSurname);
+                //add verification code to TextView
+                TextView forCode = new TextView(this);
+                forCode.setText(outgoingCode);
+                //TextView to tell user awaiting response
+                TextView forAction = new TextView(this);
+                forAction.setText("Awaiting " + outgoingUsername + "'s Response");
+                /*//add connect button
+                Button connect = new Button(this);
+                connect.setText("Connect");
+                connect.setTextColor(Color.WHITE);
+                connect.setBackgroundColor(getResources().getColor(R.color.header));
+                connect.setPadding(5, 5, 5, 5);
+                connect.setOnClickListener(connectOnClick(connect, resultUsername));*/
+
+                //add the views to the row
+                row.addView(forUsername);
+                row.addView(forFirstName);
+                row.addView(forSurname);
+                row.addView(forCode);
+                row.addView(forAction);
+
+                searchTable.addView(row, i + 1);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        rowOfTable = outgoing.length();
+    }
+
+    private void printIncomingConnections(JSONArray incoming){
+        TableLayout searchTable = (TableLayout) findViewById(R.id.connectionsTable);
+
+        TableRow incomingHeadRow = new TableRow(this);
+        incomingHeadRow.setBackgroundColor(getResources().getColor(R.color.header));
+        TextView incomingConnections = new TextView(this);
+        incomingConnections.setText("Incoming Connections");
+        incomingConnections.setTextColor(Color.WHITE);
+
+        incomingHeadRow.addView(incomingConnections);
+        searchTable.addView(incomingHeadRow, rowOfTable + 1);
+        //update row of table
+        rowOfTable += 1;
+
+
+        for (int i = 0; i < incoming.length(); i++) {
+            try {
+                JSONObject obj = incoming.getJSONObject(i);
+                System.out.println(obj);
+                String incomingUsername = obj.getString("username");
+                String incomingFirstName = obj.getString("firstname");
+                String incomingSurname = obj.getString("surname");
+                //add button
+
+
+                TableRow row = new TableRow(this);
+                //add username to TextView
+                TextView forUsername = new TextView(this);
+                forUsername.setText(incomingUsername);
+                //add first name to TextView
+                TextView forFirstName = new TextView(this);
+                forFirstName.setText(incomingFirstName);
+                //add surname to TextView
+                TextView forSurname = new TextView(this);
+                forSurname.setText(incomingSurname);
+                //add connect button
+                Button connect = new Button(this);
+                connect.setText("Connect");
+                connect.setTextColor(Color.WHITE);
+                connect.setBackgroundColor(getResources().getColor(R.color.header));
+                connect.setPadding(5, 5, 5, 5);
+                //add what to do on click
+
+                //add the views to the row
+                row.addView(forUsername);
+                row.addView(forFirstName);
+                row.addView(forSurname);
+                row.addView(connect);
+
+                searchTable.addView(row, rowOfTable + i + 1);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        rowOfTable += incoming.length();
     }
 }
