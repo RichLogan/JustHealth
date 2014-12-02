a9e6a701bcc753dd86c1002abbd32e278b8ba621
charlottehutchinson <ch446@kent.ac.uk>
2014-12-02 16:04:01 +0000

    delete appointment

--- Website/justHealthServer/api.py
+++ Website/justHealthServer/api.py
@@ -649,6 +649,18 @@ def getUpcomingAppointments(user):
 
   return json.dumps(jsonResult)
 
+@app.route('/api/deleteAppointment', methods=['POST'])
+def deleteAppointment():
+    return appointmentMedication(request.form['appid'])
+
+def deleteAppointment(appid):
+    try:
+        instance = Appointment.select().where(Appointment.appid == appid).get()
+        with database.transaction():
+            instance.delete_instance()
+        return "Deleted " + appid
+    except:
+        return appid + "not found"
 
 @app.route('/api/addMedication', methods=['POST'])
 def addMedication():
