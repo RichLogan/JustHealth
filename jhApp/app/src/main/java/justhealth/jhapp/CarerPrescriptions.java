f15b361a91356c920d6f376db08e1ec5612db86b
Rich Logan <accounts@richlogan.co.uk>
2014-12-11 16:55:33 +0000

    Links to CarerAppointments + CarerPrescriptions through MyPatients

---
+++ jhApp/app/src/main/java/justhealth/jhapp/CarerPrescriptions.java
@@ -0,0 +1,11 @@
+package justhealth.jhapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CarerPrescriptions extends Activity{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.carer_prescriptions);
+    }
+}
