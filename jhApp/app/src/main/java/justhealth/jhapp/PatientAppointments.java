681a5274074ba260869c7c573c2509928c64b8fe
Stephen Tate <accounts@sjtate.co.uk>
2014-12-14 18:17:08 +0000

    adding the appointments will add them to the database as well as the native calendar

--- jhApp/app/src/main/java/justhealth/jhapp/PatientAppointments.java
+++ jhApp/app/src/main/java/justhealth/jhapp/PatientAppointments.java
@@ -135,6 +135,54 @@ public class PatientAppointments extends Activity {
         appointmentType.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, populateSpinner));
     }
 
+    private void getUpcomingAppointments() {
+        //this will not work when API authentication is put in place
+        SharedPreferences account = getSharedPreferences("account", 0);
+        String username = account.getString("username", null);
+        String password = account.getString("password", null);
+
+        HashMap<String, String> details = new HashMap<String, String>();
+
+        //Text Boxes
+        details.put("loggedInUser", username);
+        details.put("targetUser", username);
+        String postRequest = PostRequest.post("getAllAppointments", details);
+
+        JSONArray getApps = null;
+        try {
+            getApps = new JSONArray(postRequest);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        int forUntil;
+
+        if (getApps.length() < 5) {
+            forUntil = getApps.length();
+        }
+        else {
+            forUntil = 5;
+        }
+        assert getApps != null;
+        for (int i = 0; i < forUntil; i++) {
+            try {
+                JSONObject obj = getApps.getJSONObject(i);
+                String name = obj.getString("name");
+                String startDate = obj.getString("startdate");
+                String startTime = obj.getString("starttime");
+
+                Button app = new Button(this);
+                app.setText(name + " " + startDate + " " + startTime);
+                LinearLayout layout = (LinearLayout) findViewById(R.id.upcomingAppointmentView);
+                layout.addView(app);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 
     private void createApp() {
 
@@ -159,118 +207,86 @@ public class PatientAppointments extends Activity {
         final String appType = String.valueOf(appTypeSpinner.getSelectedItem());
         details.put("apptype", appType);
 
-        if (((CheckBox) findViewById(R.id.appPrivate)).isChecked()) {
-            details.put("private", "TRUE");
+        if (((CheckBox) findViewById(R.id.appPrivate)).isChecked() == true) {
+            details.put("private", "True");
         } else {
-            details.put("private", "FALSE");
+            details.put("private", "False");
         }
 
-        HttpClient httpclient = new DefaultHttpClient();
-        String authentication = username + ":" + password;
-        String encodedAuthentication = Base64.encodeToString(authentication.getBytes(), Base64.NO_WRAP);
+        String responseString = PostRequest.post("addPatientAppointment", details);
 
-        HttpPost httppost = new HttpPost("http://raptor.kent.ac.uk:5000/api/addPatientAppointment");
-        httppost.setHeader("Authorization", "Basic " + encodedAuthentication);
-        try {
-            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+        System.out.println(responseString);
 
-            Set<Map.Entry<String, String>> detailsSet = details.entrySet();
-            for (Map.Entry<String, String> string : detailsSet) {
-                nameValuePairs.add(new BasicNameValuePair(string.getKey(), string.getValue()));
-                System.out.println(nameValuePairs);
-            }
-
-            //pass the list to the post request
-            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-            System.out.println(httppost);
-            HttpResponse response = httpclient.execute(httppost);
-            String responseString = EntityUtils.toString(response.getEntity());
-
-            System.out.println(responseString);
-
-            //Check if the Layout already exists
-            LinearLayout alert = (LinearLayout) findViewById(R.id.successMessage);
-            if (alert == null) {
-                //Insert the alert message
-                LinearLayout insertAlert = (LinearLayout) findViewById(R.id.insertAlert);
-                View insertAlertView = getLayoutInflater().inflate(R.layout.success_message, insertAlert, false);
-                insertAlert.addView(insertAlertView);
-            }
-
+        //Check if the Layout already exists
+        LinearLayout alert = (LinearLayout) findViewById(R.id.successMessage);
+        if (alert == null) {
+            //Insert the alert message
+            LinearLayout insertAlert = (LinearLayout) findViewById(R.id.insertAlert);
+            View insertAlertView = getLayoutInflater().inflate(R.layout.success_message, insertAlert, false);
+            insertAlert.addView(insertAlertView);
             TextView myTextView = (TextView) findViewById(R.id.successText);
-            myTextView.setText(responseString);
 
-        } catch (ClientProtocolException e) {
-            e.printStackTrace();
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    private void getUpcomingAppointments() {
-        //this will not work when API authentication is put in place
-        SharedPreferences account = getSharedPreferences("account", 0);
-        String username = account.getString("username", null);
-        String password = account.getString("password", null);
-
-        HashMap<String, String> details = new HashMap<String, String>();
-
-        //Text Boxes
-        details.put("loggedInUser", username);
-        details.put("targetUser", username);
-        String postRequest = PostRequest.post("getAllAppointments", details);
-
-        JSONArray getApps = null;
-        try {
-            getApps = new JSONArray(postRequest);
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-        assert getApps != null;
-        for (int i = 0; i < 5; i++) {
-            try {
-                JSONObject obj = getApps.getJSONObject(i);
-                String name = obj.getString("name");
-                String startDate = obj.getString("startdate");
-                String startTime = obj.getString("starttime");
-
-                Button app = new Button(this);
-                app.setText(name + " " + startDate + " " + startTime);
-                LinearLayout layout = (LinearLayout) findViewById(R.id.upcomingAppointmentView);
-                layout.addView(app);
-
-            } catch (JSONException e) {
-                e.printStackTrace();
+            if (responseString.equals("Appointment Added")) {
+                myTextView.setText(responseString);
+                addToCalendar(details);
+            }
+            else {
+                myTextView.setText("Oops something went wrong, try again.");
             }
         }
     }
 
-
-    /* Calendar example
-
+    //Calendar example
     @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
-    private void calendar() {
-        Calendar beginTime = Calendar.getInstance();
-        beginTime.set(2014, 12, 25, 9, 50);
-        Calendar endTime = Calendar.getInstance();
-        endTime.set(2014, 12, 25, 17, 30);
+    private void addToCalendar(HashMap<String, String> details) {
+        String appName = details.get("name");
+        String location = details.get("addressnamenumber") + ", " + details.get("postcode");
+        String startDate = details.get("startdate");
+        String startTime = details.get("starttime");
+        String endDate = details.get("enddate");
+        String endTime = details.get("endtime");
+        //String description = details.get("description");
+
+        //get the correct format of the start date/time of the calendar appointment
+        HashMap<String, Integer> appStart = getDateTimeFormat(startDate, startTime);
+        System.out.println(appStart);
+
+        //get the correct format of the end date/time of the calendar appointment
+        HashMap<String, Integer> appEnd = getDateTimeFormat(endDate, endTime);
+        System.out.println(appEnd);
+
+        Calendar start = Calendar.getInstance();
+        start.set(appStart.get("year"), appStart.get("month"), appStart.get("day"), appStart.get("hour"), appStart.get("minute"));
+        Calendar end = Calendar.getInstance();
+        end.set(appEnd.get("year"), appEnd.get("month"), appEnd.get("day"), appEnd.get("hour"), appEnd.get("minute"));
 
         Intent intent = new Intent(Intent.ACTION_INSERT)
                 .setData(CalendarContract.Events.CONTENT_URI)
-                .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, beginTime.getTimeInMillis())
-                .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, endTime.getTimeInMillis())
-                .putExtra(CalendarContract.Events.TITLE, "Christmas Day Shananigans")
-                .putExtra(CalendarContract.Events.EVENT_LOCATION, "SS17 9AY");
+                .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, start.getTimeInMillis())
+                .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, end.getTimeInMillis())
+                .putExtra(CalendarContract.Events.TITLE, appName)
+                .putExtra(CalendarContract.Events.EVENT_LOCATION,location);
         startActivity(intent);
+    }
 
-        System.out.println("Event ID: " + CalendarContract.Events.CONTENT_URI);
-    }*/
+    private HashMap<String, Integer> getDateTimeFormat(String date, String time) {
+        HashMap<String, Integer> formattedDateTime = new HashMap<>();
 
+        System.out.println(date);
+        System.out.println(time);
+        Integer year = Integer.parseInt(date.substring(0,4));
+        Integer month = Integer.parseInt(date.substring(5,7));
+        month -= 1;  //because January = 0... December = 11
+        Integer day = Integer.parseInt(date.substring(8,10));
+        Integer hour = Integer.parseInt(time.substring(0,2));
+        Integer minute = Integer.parseInt(time.substring(3,5));
 
+        formattedDateTime.put("year", year);
+        formattedDateTime.put("month", month);
+        formattedDateTime.put("day", day);
+        formattedDateTime.put("hour", hour);
+        formattedDateTime.put("minute", minute);
+        return formattedDateTime;
+    }
 
 }
