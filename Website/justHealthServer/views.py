28e96de6d7d7421eaeb0484b025637df0b6b063c
charlottehutchinson <ch446@kent.ac.uk>
2015-01-24 16:03:31 +0000

    contact us update

--- Website/justHealthServer/views.py
+++ Website/justHealthServer/views.py
@@ -87,6 +87,14 @@ def faq():
 def sitemap():
     return render_template('sitemap.html')
 
+@app.route('/contactUs')
+def contactUs():
+    profileDetails = json.loads(getAccountInfo(session['username']))
+    if profileDetails['accounttype'] == "Patient":
+        return render_template('contactUs.html', profileDetails=profileDetails, printaccounttype = 'Patient')
+    elif profileDetails['accounttype'] == "Carer":
+        return render_template('contactUs.html', profileDetails=profileDetails, printaccounttype = 'Carer' )  
+
 @app.route('/settings')
 @needLogin
 def settings():
