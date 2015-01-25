cd55edd2cdfddc61f4e5e22121492e9b80229c69
R.Logan <rl260@raptor.kent.ac.uk>
2015-01-25 17:58:33 +0000

    TLS1.2 Implemented. Not redirecting at the moment

--- Website/justHealthServer/__init__.py
+++ Website/justHealthServer/__init__.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask, flash, render_template, request, session, redirect, url_for, abort
+#from flask_sslify import SSLify
 app = Flask(__name__)
+#sslify = SSLify(app)
 app.secret_key = '^\x83J\xd3) \x1a\xa4\x05\xea\xd8,\t=\x14]\xfd\x8c%\x90\xd6\x9f\xa1Z'
 
 # Import Modules
