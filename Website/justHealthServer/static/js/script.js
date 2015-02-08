a282e791a33fe484631753bf9fb04148a33e27e1
benmcgregor <ben.mcgregor1@gmail.com>
2015-02-08 23:46:55 +0000

    Validation progress

--- Website/justHealthServer/static/js/script.js
+++ Website/justHealthServer/static/js/script.js
@@ -17,12 +17,6 @@ function validateEmail(email) {
 	return re.test(email);
 }
 
-//check date of birth is correct format
-function validateDOB(dob) {
-	var pattern = /^(((0[1-9]|[12]\d|3[01])\/(0[13578]|1[02])\/((19|[2-9]\d)\d{2}))|((0[1-9]|[12]\d|30)\/(0[13456789]|1[012])\/((19|[2-9]\d)\d{2}))|((0[1-9]|1\d|2[0-8])\/02\/((19|[2-9]\d)\d{2}))|(29\/02\/((1[6-9]|[2-9]\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00))))$/g;
-	return pattern.test(dob);
-}
-
 //password checking function
 //(http://runnable.com/UfJrnXtk2tZXAAA1/how-to-check-password-strength-using-jquery)
 function checkStrength(password) {
