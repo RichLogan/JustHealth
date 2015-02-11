02363c1eff841cf7f4c6774a00fb47fb1c177df8
Rich Logan <accounts@richlogan.co.uk>
2015-02-11 15:46:23 +0000

    Fixes #151

--- jhApp/app/src/main/java/justhealth/jhapp/Search.java
+++ jhApp/app/src/main/java/justhealth/jhapp/Search.java
@@ -62,12 +62,16 @@ public class Search extends Activity {
         searchInformation.put("searchterm", ((EditText) findViewById(R.id.searchField)).getText().toString());
 
         String response = Request.post("searchPatientCarer", searchInformation, getApplicationContext());
+        if (response.equals("No users found")) {
+            Feedback.toast("No users found matching your search query", false, getApplicationContext());
+            return;
+        }
         try {
             JSONArray queryReturn = new JSONArray(response);
             printTable(queryReturn);
         }
         catch (JSONException e) {
-            System.out.println(e.getStackTrace());
+            Feedback.toast("Could not load search results", false, getApplicationContext());
         }
     }
 
