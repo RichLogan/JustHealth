c6869d4eed13dc1518c5336ac86ae0778713766e
benmcgregor <ben.mcgregor1@gmail.com>
2015-02-18 23:38:02 +0000

    Connections table edits

--- Website/justHealthServer/views.py
+++ Website/justHealthServer/views.py
@@ -65,6 +65,7 @@ def index():
             # Get all patients connected to this user
             connections = json.loads(getConnections(session['username']))
             completedConnections = json.loads(connections['completed'])
+
             patients = []
             for connection in completedConnections:
                 if connection['accounttype'] == "Patient":
