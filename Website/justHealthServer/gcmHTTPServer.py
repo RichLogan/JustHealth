c53702e85106f14030cb62dd5ec43c2e4b09124d
Rich Logan <accounts@richlogan.co.uk>
2015-02-26 17:56:31 +0000

    Push notification function

--- Website/justHealthServer/gcmHTTPServer.py
+++ Website/justHealthServer/gcmHTTPServer.py
@@ -1,14 +1,31 @@
-import requests, json
-headers = {
-	'content-type' : 'application/json',
-	'Authorization' : "key=AIzaSyCLEJR4te1Y6i2hJa7mOhkeFkTzBbWfZ9Y"
+from justHealthServer import *
+from api import *
+import json
+import requests
 
-payload = {
-	"collapse_key" : "JustHealth",
-	"data" : {
-		"message" : "ciao"
-	},
-	"registration_ids": ['APA91bFSdGrImn8cmDmRxmp5cdxlj9cACubPJzZlOCbZPDEa5k3gOW6T1tKbATFRR3L6F62W22X8c-TBKBbiV1sSdGrr78cx4gkyKwtGUT6UL7U5KSQ4d1h35kSOrE5AYFo5QXN_dyKTNqBg87SspZeh1vCEFLKrhw']
-}
-r = requests.post('https://android.googleapis.com/gcm/send', data=json.dumps(payload), headers=headers)
-print r.text
+def sendPushNotification(username, title, message):
+  """Sends a push notification to the users Android device with the specified title and message"""
+  # Get Registration ID
+  registrationid = Androidregistration.select().where(Androidregistration.username == username).get().registrationid
+
+  # Build Message
+  headers = {
+  'content-type' : 'application/json',
+  'Authorization' : "key=" + app.config['GCM_API_KEY']
+  }
+  payload = {
+    "data" : {
+      "title" : title,
+      "message" : message
+    },
+    "registration_ids": [registrationid]
+  }
+  
+  # Post to google
+  r = requests.post('https://android.googleapis.com/gcm/send', data=json.dumps(payload), headers=headers)
+
+  # Check result
+  result = json.loads(r.text)
+  if (result['success'] == 1) and (result['failure'] == 0):
+    return True
+  return False
