1331cad03be9429f646a37397c19487ec805ae02
Rich Logan <accounts@richlogan.co.uk>
2015-03-14 12:38:23 +0000

    ConnectionsMain changed to Async

--- jhApp/app/src/main/java/justhealth/jhapp/ConnectionsMain.java
+++ jhApp/app/src/main/java/justhealth/jhapp/ConnectionsMain.java
@@ -4,6 +4,7 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -73,28 +74,36 @@ public class ConnectionsMain extends Activity {
 
         // Load Connections
         getConnections();
-        loadBadges();
     }
 
     /**
      * Retrieves ALL connections from the database in order for counts to be calculated and displayed via loadBadges()
      */
     private void getConnections() {
-        HashMap<String, String> getConnectionsInfo = new HashMap<String, String>();
+        final HashMap<String, String> getConnectionsInfo = new HashMap<String, String>();
         SharedPreferences account = getSharedPreferences("account", 0);
         String username = account.getString("username", null);
         getConnectionsInfo.put("username", username);
 
-        String response = Request.post("getConnections", getConnectionsInfo, getApplicationContext());
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... v) {
+                String response = Request.post("getConnections", getConnectionsInfo, getApplicationContext());
+                try {
+                    JSONObject result = new JSONObject(response);
+                    outgoing = new JSONArray(result.getString("outgoing"));
+                    incoming = new JSONArray(result.getString("incoming"));
+                    completed = new JSONArray(result.getString("completed"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
 
-        try {
-            JSONObject result = new JSONObject(response);
-            outgoing = new JSONArray(result.getString("outgoing"));
-            incoming = new JSONArray(result.getString("incoming"));
-            completed = new JSONArray(result.getString("completed"));
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+            protected void onPostExecute(Void v) {
+                loadBadges();
+            }
+        }.execute();
     }
 
     /**
