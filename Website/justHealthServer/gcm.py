903400264eb46b5753247325a269959137b9c83f
Stephen Tate <accounts@sjtate.co.uk>
2015-03-20 13:42:54 +0000

    update gcm testing

--- Website/justHealthServer/gcm.py
+++ Website/justHealthServer/gcm.py
@@ -237,4 +237,4 @@ def getAndroidNotificationContent(notification):
 def testPushNotification():
   for user in Androidregistration.select():
     sendPushNotification(user.username, "Test Push", "This is a test push notification that has been sent manually by the JustHealth team. This is for demonstration only")
-    return "You should have recieved a push notification!"
+    print "You should have recieved a push notification!"
