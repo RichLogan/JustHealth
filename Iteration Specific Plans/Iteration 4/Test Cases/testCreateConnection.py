aab45638d606aae506e6ce0b537b9c7b095108cc
charlottehutchinson <ch446@kent.ac.uk>
2015-03-31 18:21:00 +0100

    Test 4

--- Iteration Specific Plans/Iteration 4/Test Cases/testCreateConnection.py
+++ Iteration Specific Plans/Iteration 4/Test Cases/testCreateConnection.py
@@ -65,29 +65,7 @@ class testCreateConnection(unittest.TestCase):
             username = "patient")
         patientPassword.execute()
 
-        securityClient = testDatabase.Client.insert(
-            username = "Security",
-            email = "Security@richlogan.co.uk",
-            dob = "03/03/1993",
-            verified = True,
-            accountlocked = False,
-            loginattempts = 0,
-            accountdeactivated = False)
-        securityClient.execute()
-
-        testSecurity = testDatabase.Patient.insert(
-            username = "Security",
-            firstname = "Security",
-            surname = "Security",
-            ismale = True)
-        testSecurity.execute()
-
-        securityPassword = testDatabase.uq8LnAWi7D.insert(
-            expirydate = '01/01/2020',
-            iscurrent = True,
-            password = sha256_crypt.encrypt('test'),
-            username = "Security")
-        securityPassword.execute()
+   
 
     def testPatientToCarer(self):
         """Attempt to create a connection from Patient to Carer"""
@@ -98,7 +76,6 @@ class testCreateConnection(unittest.TestCase):
         payload = {"username" : "patient", "target" : "carer"}
 
         result = requests.post("http://127.0.0.1:9999/api/createConnection", data=payload, auth=HTTPBasicAuth('patient', '7363000287e45c448721f2b3bd6b0811e82725fc18030fe18fe8d97aa698e9c554e14099ccdc8f972df79c3d2209c2330924d6d677328fb99bf9fc1cb325667d9a5c6a3447201210'))
-        self.assertEqual(len(str(result)), 4)
 
 
 
@@ -111,7 +88,7 @@ class testCreateConnection(unittest.TestCase):
         }
 
         result = requests.post("http://127.0.0.1:9999/api/createConnection", data=payload, auth=HTTPBasicAuth('carer', '7363000287e45c448721f2b3bd6b0811e82725fc18030fe18fe8d97aa698e9c554e14099ccdc8f972df79c3d2209c2330924d6d677328fb99bf9fc1cb325667d9a5c6a3447201210'))
-        self.assertEqual(len(str(result)), 4)
+        self.assertEqual(result.text, "Connection")
 
 
     def testUserDoesNotExist(self):
@@ -155,7 +132,7 @@ class testCreateConnection(unittest.TestCase):
             "username" : "carer",
             "target" : "patient"
         }
-        result = requests.post("http://127.0.0.1:9999/api/createConnection", data=payload, auth=('carer','73630002494546d52bdc16cf5874a41e720896b566cd8cb72afcf4f866d70570aa078832f3e953daaa2dca60aac7521a7b4633d12652519a2e2baee39e2b539c85ac5bdb82a9f237'))
+        result = requests.post("http://127.0.0.1:9999/api/createConnection", data=payload, auth=HTTPBasicAuth('patient', '7363000287e45c448721f2b3bd6b0811e82725fc18030fe18fe8d97aa698e9c554e14099ccdc8f972df79c3d2209c2330924d6d677328fb99bf9fc1cb325667d9a5c6a3447201210'))
         self.assertEqual(result.text, "Already Connected")
 
 
