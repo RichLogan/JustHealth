99ce97ac85c99bf3e1aba72ff84763359530c42b
Rich Logan <accounts@richlogan.co.uk>
2015-04-01 10:34:24 +0100

    All

--- runTests.sh
+++ runTests.sh
@@ -82,100 +82,137 @@ sed -i "" -e "2s/.*/from database import */" Website/justHealthServer/gcm.py;
 function runTests {
   echo -e "---------------Just Health Testing Suite---------------"
 
-  # echo "Iteration 1"
-  # python -m unittest discover Iteration\ 1/Test\ Cases/Testing\ Scripts/
-  # echo -e "\n"
+  echo -e "\n"
 
-  # echo "Iteration 2"
-  # # testPath
-  # echo -e "\n"
+  echo -e "======================================================================"
+  echo "Iteration 4"
+  echo -e "======================================================================"
 
-  # echo "Iteration 3"
-  # # testPath
-  # echo -e "\n"
+    echo "Authentication"
+    python -m unittest discover Iteration\ Specific\ Plans/Iteration\ 4/Test\ Cases/ testAuthentication.py
+    echo -e "\n"
 
-  # echo "Iteration 4"
-  # echo "--"
-  #   echo "Search Patient Carer"
-  #   python -m unittest discover Iteration\ 4/Test\ Cases/ testSearchPatientCarer.py
-  
-  #   echo "Create Connection"
-  #   python -m unittest discover Iteration\ 4/Test\ Cases/ testCreateConnection.py
-    
-  #   # echo "Complete Connection"
-  #   # python -m unittest discover Iteration\ 4/Test\ Cases/ testCompleteConnection.py
-    
-  #   # echo "Delete Connection"
-  #   # python -m unittest discover Iteration\ 4/Test\ Cases/ testDeleteConnection.py
-    
-  #   # echo "Cancel Connection"
-  #   # python -m unittest discover Iteration\ 4/Test\ Cases/ testCancelConnection.py
-    
-  #   # echo "Get Connections"
-  #   # python -m unittest discover Iteration\ 4/Test\ Cases/ testGetConnections.py
-  # echo -e "\n"
+    echo "Cancel Connection"
+    python -m unittest discover Iteration\ Specific\ Plans/Iteration\ 4/Test\ Cases/ testCancelConnection.py
+    echo -e "\n"
 
+    echo "Complete Connection"
+    python -m unittest discover Iteration\ Specific\ Plans/Iteration\ 4/Test\ Cases/ testCompleteConnection.py
+    echo -e "\n"
+
+    echo "Create Connection"
+    python -m unittest discover Iteration\ Specific\ Plans/Iteration\ 4/Test\ Cases/ testCreateConnection.py
+    echo -e "\n"
+
+    echo "Deactivate Account"
+    python -m unittest discover Iteration\ Specific\ Plans/Iteration\ 4/Test\ Cases/ testDeactivateAccount.py
+    echo -e "\n"
+
+    echo "Delete Connection"
+    python -m unittest discover Iteration\ Specific\ Plans/Iteration\ 4/Test\ Cases/ testDeleteConnection.py
+    echo -e "\n"
+
+    echo "Get Account Info"
+    python -m unittest discover Iteration\ Specific\ Plans/Iteration\ 4/Test\ Cases/ testGetAccountInfo.py
+    echo -e "\n"
+
+    echo "Get Connections"
+    python -m unittest discover Iteration\ Specific\ Plans/Iteration\ 4/Test\ Cases/ testGetConnections.py
+    echo -e "\n"
+
+    echo "Registration"
+    python -m unittest discover Iteration\ Specific\ Plans/Iteration\ 4/Test\ Cases/ testRegistration.py
+    echo -e "\n"
+
+    echo "Search Patient Carer"
+    python -m unittest discover Iteration\ Specific\ Plans/Iteration\ 4/Test\ Cases/ testSearchPatientCarer.py
+    echo -e "\n"
+
+  echo -e "======================================================================"
   echo "Iteration 5"
-  echo "--"
+  echo -e "======================================================================"
 
-    # echo "Add Appointments"
-    # python -m unittest discover Iteration\ Specific\ Plans/Iteration\ 5/Test\ Cases/ testAddAppointments.py
+    echo "Add Appointments"
+    python -m unittest discover Iteration\ Specific\ Plans/Iteration\ 5/Test\ Cases/ testAddAppointments.py
+    echo -e "\n"
 
-    # echo "Add Medication"
-    # python -m unittest discover Iteration\ Specific\ Plans/Iteration\ 5/Test\ Cases/ testAddMedication.py
+    echo "Add Medication"
+    python -m unittest discover Iteration\ Specific\ Plans/Iteration\ 5/Test\ Cases/ testAddMedication.py
+    echo -e "\n"
 
-    # echo "Add Prescription"
-    # python -m unittest discover Iteration\ Specific\ Plans/Iteration\ 5/Test\ Cases/ testAddPrescription.py
+    echo "Add Prescription"
+    python -m unittest discover Iteration\ Specific\ Plans/Iteration\ 5/Test\ Cases/ testAddPrescription.py
+    echo -e "\n"
 
-    # echo "Delete Appointment"
-    # python -m unittest discover Iteration\ Specific\ Plans/Iteration\ 5/Test\ Cases/ testDeleteAppointments.py
+    echo "Delete Appointment"
+    python -m unittest discover Iteration\ Specific\ Plans/Iteration\ 5/Test\ Cases/ testDeleteAppointments.py
+    echo -e "\n"
 
-    # echo "Delete Medication"
-    # python -m unittest discover Iteration\ Specific\ Plans/Iteration\ 5/Test\ Cases/ testDeleteMedication.py
+    echo "Delete Medication"
+    python -m unittest discover Iteration\ Specific\ Plans/Iteration\ 5/Test\ Cases/ testDeleteMedication.py
+    echo -e "\n"
 
-    # echo "Delete Prescription"
-    # python -m unittest discover Iteration\ Specific\ Plans/Iteration\ 5/Test\ Cases/ testDeletePrescription.py
+    echo "Delete Prescription"
+    python -m unittest discover Iteration\ Specific\ Plans/Iteration\ 5/Test\ Cases/ testDeletePrescription.py
+    echo -e "\n"
     
     echo "Get Appointment"
     python -m unittest discover Iteration\ Specific\ Plans/Iteration\ 5/Test\ Cases/ testGetAppointments.py
-  
-  # echo "Iteration 8"
-  # echo "--"    
+    echo -e "\n"
 
-  #   echo "Get Notifications"
-  #   python -m unittest discover Iteration\ Specific\ Plans/Iteration\ 8/TestCases/ testGetNotifications.py
+    echo "Get Prescriptions"
+    python -m unittest discover Iteration\ Specific\ Plans/Iteration\ 5/Test\ Cases/ testGetPrescriptions.py
+    echo -e "\n"
 
-  #   echo "Add Reminders"
-  #   python -m unittest discover Iteration\ Specific\ Plans/Iteration\ 8/TestCases/ testAddReminders.py
+  echo -e "======================================================================"
+  echo "Iteration 8"
+  echo -e "======================================================================"
 
-  #   echo "Create Notification"
-  #   python -m unittest discover Iteration\ Specific\ Plans/Iteration\ 8/TestCases/ testCreateNotification.py
+    echo "Get Notifications"
+    python -m unittest discover Iteration\ Specific\ Plans/Iteration\ 8/TestCases/ testGetNotifications.py
+    echo -e "\n"
+
+    echo "Add Reminders"
+    python -m unittest discover Iteration\ Specific\ Plans/Iteration\ 8/TestCases/ testAddReminders.py
+    echo -e "\n"
+
+    echo "Create Notification"
+    python -m unittest discover Iteration\ Specific\ Plans/Iteration\ 8/TestCases/ testCreateNotification.py
+    echo -e "\n"
     
-  #   echo "Create Prescription Instances"
-  #   python -m unittest discover Iteration\ Specific\ Plans/Iteration\ 8/TestCases/ testCreatePrescriptionInstances.py
+    echo "Create Prescription Instances"
+    python -m unittest discover Iteration\ Specific\ Plans/Iteration\ 8/TestCases/ testCreatePrescriptionInstances.py
+    echo -e "\n"
     
-  #   echo "Delete Reminders"
-  #   python -m unittest discover Iteration\ Specific\ Plans/Iteration\ 8/TestCases/ testDeleteReminders.py
+    echo "Delete Reminders"
+    python -m unittest discover Iteration\ Specific\ Plans/Iteration\ 8/TestCases/ testDeleteReminders.py
+    echo -e "\n"
 
-  #   echo "Dismiss Notifications"
-  #   python -m unittest discover Iteration\ Specific\ Plans/Iteration\ 8/TestCases/ testDimissNotification.py
+    echo "Dismiss Notifications"
+    python -m unittest discover Iteration\ Specific\ Plans/Iteration\ 8/TestCases/ testDimissNotification.py
+    echo -e "\n"
 
-  #   echo "Get appointments due in 30 minutes"
-  #   python -m unittest discover Iteration\ Specific\ Plans/Iteration\ 8/TestCases/ testGetAppointmentsDueIn30.py
+    echo "Get appointments due in 30 minutes"
+    python -m unittest discover Iteration\ Specific\ Plans/Iteration\ 8/TestCases/ testGetAppointmentsDueIn30.py
+    echo -e "\n"
 
-  #   echo "Get appointments due now"
-  #   python -m unittest discover Iteration\ Specific\ Plans/Iteration\ 8/TestCases/ testGetAppointmentsDueNow.py
+    echo "Get appointments due now"
+    python -m unittest discover Iteration\ Specific\ Plans/Iteration\ 8/TestCases/ testGetAppointmentsDueNow.py
+    echo -e "\n"
 
-  #   echo "Get minutes difference"
-  #   python -m unittest discover Iteration\ Specific\ Plans/Iteration\ 8/TestCases/ testGetMinutesDifference.py  
+    echo "Get minutes difference"
+    python -m unittest discover Iteration\ Specific\ Plans/Iteration\ 8/TestCases/ testGetMinutesDifference.py
+    echo -e "\n"
 
-  #   echo "Get Missed Prescriptions"
-  #   python -m unittest discover Iteration\ Specific\ Plans/Iteration\ 8/TestCases/ testGetMissedPrescriptions.py    
+    echo "Get Missed Prescriptions"
+    python -m unittest discover Iteration\ Specific\ Plans/Iteration\ 8/TestCases/ testGetMissedPrescriptions.py
+    echo -e "\n"
 
-  #   echo "Get Prescriptions Due Today"
-  #   python -m unittest discover Iteration\ Specific\ Plans/Iteration\ 8/TestCases/ testGetPrescriptionDueToday.py 
+    echo "Get Prescriptions Due Today"
+    python -m unittest discover Iteration\ Specific\ Plans/Iteration\ 8/TestCases/ testGetPrescriptionDueToday.py
+    echo -e "\n"
 
-  #   echo "Get Reminders"
-  #   python -m unittest discover Iteration\ Specific\ Plans/Iteration\ 8/TestCases/ testGetReminders.py    
-
+    echo "Get Reminders"
+    python -m unittest discover Iteration\ Specific\ Plans/Iteration\ 8/TestCases/ testGetReminders.py 
+  echo -e "\n"
 }
